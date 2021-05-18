892c5eefcfe741c58d07c9f0edd8a905b8066e0d
Kacper <kacper.plusaa@gmail.com>
2021-05-18 20:29:52 +0200

    added get orders (all and via id)

--- api/server.js
+++ api/server.js
@@ -1,52 +1,108 @@
-const dbconfig = require('./dbconfig')
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const con =dbconfig.connect()
-app.use(cors())
-app.use(express.urlencoded({extended: true})); 
-app.use(express.json());   
+const dbconfig = require("./dbconfig");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const con = dbconfig.connect();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.json());
-app.get('/status', function (req, res) {
+app.get("/", function (req, res) {
   res.json({
-      'status': 'OK'
-  })
-  
-})
-app.post('/create-order', function (req, res)
-{
-  var sql="";
-  var sum= countSum(req.body.pizzaType, req.body.pizzaSize, req.body.amountOfPizzas);
+    status: "OK",
+  });
+});
+app.post("/create-order", function (req, res) {
+  var sql = "";
+  var sum = countSum(
+    req.body.pizzaType,
+    req.body.pizzaSize,
+    req.body.amountOfPizzas
+  );
   var deliveryFee = 5;
-  if(req.body.delivery=="no")
-  sql= "INSERT INTO pizza_order ( delivery, pizza_type, pizza_size, pizza_dough, pizza_sauce, amount_of_pizzas) VALUES ('"+req.body.delivery+"', '"+req.body.pizzaType+"', '"+req.body.pizzaSize+"','"+ req.body.pizzaDough+"','"+req.body.pizzaSauce+"','"+ req.body.amountOfPizzas+"')";
-else{
-  sql= "INSERT INTO pizza_order ( delivery, fullname, telephone_number, street,city, pizza_type, pizza_size, pizza_dough, pizza_sauce, amount_of_pizzas) VALUES ('"+req.body.delivery+"','"+req.body.fullname+"','"+req.body.telephoneNumber+"','"+req.body.street+"','"+req.body.city+"', '"+req.body.pizzaType+"', '"+req.body.pizzaSize+"','"+ req.body.pizzaDough+"','"+req.body.pizzaSauce+"','"+ req.body.amountOfPizzas+"')";
-  sum+=deliveryFee;
-}
-  con.query(sql,[], function(err,result)
-  {
+  if (req.body.delivery == "no")
+    sql =
+      "INSERT INTO pizza_order ( delivery, pizza_type, pizza_size, pizza_dough, pizza_sauce, amount_of_pizzas) VALUES ('" +
+      req.body.delivery +
+      "', '" +
+      req.body.pizzaType +
+      "', '" +
+      req.body.pizzaSize +
+      "','" +
+      req.body.pizzaDough +
+      "','" +
+      req.body.pizzaSauce +
+      "','" +
+      req.body.amountOfPizzas +
+      "')";
+  else {
+    sql =
+      "INSERT INTO pizza_order ( delivery, fullname, telephone_number, street,city, pizza_type, pizza_size, pizza_dough, pizza_sauce, amount_of_pizzas) VALUES ('" +
+      req.body.delivery +
+      "','" +
+      req.body.fullname +
+      "','" +
+      req.body.telephoneNumber +
+      "','" +
+      req.body.street +
+      "','" +
+      req.body.city +
+      "', '" +
+      req.body.pizzaType +
+      "', '" +
+      req.body.pizzaSize +
+      "','" +
+      req.body.pizzaDough +
+      "','" +
+      req.body.pizzaSauce +
+      "','" +
+      req.body.amountOfPizzas +
+      "')";
+    sum += deliveryFee;
+  }
+  con.query(sql, [], function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
   res.json({
-    'summ': sum,
-    'deliveryFee': deliveryFee
-  })
-  
-  
-})
-
-app.post('/send-message', function(req,res)
-{
-console.log("Message is received from name: "+req.body.name+"\nemail: "+req.body.email+"\nFollowing message: "+req.body.message)
+    summ: sum,
+    deliveryFee: deliveryFee,
+  });
 });
 
+app.get("/orders", function (req, res) {
+  var sql = "";
+  sql = "SELECT * FROM `pizza_order`";
+  con.query(sql, [], function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
 
-app.listen(3000, function(){
-    console.log("Listening")
-})
+app.get("/orders/:orderId", function (req, res) {
+  var sql = "";
+  sql = "SELECT * FROM `pizza_order` where id='" + req.params.orderId + "'";
+  con.query(sql, [], function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+app.post("/send-message", function (req, res) {
+  console.log(
+    "Message is received from name: " +
+      req.body.name +
+      "\nemail: " +
+      req.body.email +
+      "\nFollowing message: " +
+      req.body.message
+  );
+});
+
+app.listen(3000, function () {
+  console.log("Listening");
+});
 
 function countSum(type, pizzaSize, pizzaQuantity) {
   var sum = 0;
@@ -90,5 +146,3 @@ function countSum(type, pizzaSize, pizzaQuantity) {
   sum *= pizzaQuantity;
   return sum;
 }
-
-
